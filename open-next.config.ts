502e1396edbed954dc707d20ff6f0ce8c96f31de
Félix Antonio  Suira García <felix-a.suira-g@up.ac.pa>
2026-08-25 16:20:36 -0500

    Enhance open-next configuration with new settings

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,24 @@
 const config = {
-  default: {}
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "auto",
+    },
+  },
+  dangerous: {
+    enableCacheInterception: false,
+  },
 };
 
 export default config;
