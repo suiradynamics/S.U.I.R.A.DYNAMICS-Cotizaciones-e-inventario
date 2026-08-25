ceb4f7e50ef26c17febeeebe7afb7e50246c127d
Félix Antonio  Suira García <felix-a.suira-g@up.ac.pa>
2026-08-25 14:20:31 -0500

    Update setAll method to include TypeScript types

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -12,14 +12,13 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: Array<{ name: string; value: string; options?: any }>) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             );
           } catch {
-            // El método `setAll` fue llamado desde un Server Component.
-            // Esto se puede ignorar si tienes middleware refrescando sesiones.
+            // El método setAll fue llamado desde un Server Component.
           }
         },
       },
