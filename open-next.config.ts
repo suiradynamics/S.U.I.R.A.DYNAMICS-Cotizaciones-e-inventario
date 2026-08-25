a2ed6994ff64ebef7d028c7cafaabfd387aa266c
Félix Antonio  Suira García <felix-a.suira-g@up.ac.pa>
2026-08-25 16:23:48 -0500

    Refactor config to use defineCloudflareConfig

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,24 +1,3 @@
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "auto",
-    },
-  },
-  dangerous: {
-    enableCacheInterception: false,
-  },
-};
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default config;
+export default defineCloudflareConfig();
