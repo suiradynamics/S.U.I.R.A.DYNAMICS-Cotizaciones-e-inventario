ba39fd1599546218b282b352ec63ef792f45420e
Félix Antonio  Suira García <felix-a.suira-g@up.ac.pa>
2026-08-26 00:20:25 -0500

    Refactor open-next.config.ts for new configuration

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,22 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({});
+export default {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+  dangerous: {
+    enableCacheInterception: false,
+  },
+};
