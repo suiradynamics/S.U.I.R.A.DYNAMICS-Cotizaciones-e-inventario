dc23a6a8742006e8a1a4a15b0989403c02362541
Félix Antonio  Suira García <felix-a.suira-g@up.ac.pa>
2026-08-25 23:46:33 -0500

    Change Cloudflare adapter execution method

--- build.js
+++ build.js
@@ -4,6 +4,7 @@ console.log('--- INICIANDO BUILD PERSONALIZADO DE NEXT.JS ---');
 execSync('npx next build', { stdio: 'inherit' });
 
 console.log('--- EJECUTANDO ADAPTADOR DE CLOUDFLARE ---');
-execSync('npx @opennextjs/cloudflare', { stdio: 'inherit' });
+// Invocamos directamente el script principal del adaptador sin pasar por la CLI que rechaza argumentos
+require('./node_modules/@opennextjs/cloudflare/dist/index.js');
 
 console.log('--- ¡PROCESO COMPLETADO CON ÉXITO! ---');
