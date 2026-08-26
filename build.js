8089bc5e8295b79f913f10ac59522d6e73afbf18
Félix Antonio  Suira García <felix-a.suira-g@up.ac.pa>
2026-08-25 23:43:47 -0500

    Update build.js

--- build.js
+++ build.js
@@ -4,7 +4,6 @@ console.log('--- INICIANDO BUILD PERSONALIZADO DE NEXT.JS ---');
 execSync('npx next build', { stdio: 'inherit' });
 
 console.log('--- EJECUTANDO ADAPTADOR DE CLOUDFLARE ---');
-// Ejecutamos directamente el archivo binario del paquete instalado
-execSync('node node_modules/@opennextjs/cloudflare/dist/cli/index.js build', { stdio: 'inherit' });
+execSync('npx @opennextjs/cloudflare', { stdio: 'inherit' });
 
 console.log('--- ¡PROCESO COMPLETADO CON ÉXITO! ---');
