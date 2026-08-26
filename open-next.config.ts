ceafd21eeafc59095fdcef8cb9b6228e223c580e
Félix Antonio  Suira García <felix-a.suira-g@up.ac.pa>
2026-08-25 22:16:11 -0500

    Refactor OpenNextConfig to use satisfies syntax

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,15 +1,3 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
-
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare",
-      converter: "edge",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-};
-
-export default config;
+export default {
+  default: {},
+} satisfies import("@opennextjs/cloudflare").OpenNextConfig;
